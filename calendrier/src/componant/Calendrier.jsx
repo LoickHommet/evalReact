7df3cfc362c62641e3319128d1dade8013368fe4
LoickHommet <hommet.loick@gmail.com>
2022-03-11 14:47:23 +0100

    fin

--- calendrier/src/componant/Calendrier.jsx
+++ calendrier/src/componant/Calendrier.jsx
@@ -1,60 +1,44 @@
 import { useState } from "react"
+import { useCalendrier } from "../hook/useCalendrier"
 
-
- const month = ['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre']
 
 export function Calendrier(){
-    const [date, setDate] = useState(new Date());
-    console.log(getDaysInMonth(date.getMonth(), date.getFullYear()));
+
+    const { calendarRows, selectedDate, todayFormatted, getNextMonth, getPrevMonth,jour,mois,getNextYear, getPrevYear, } = useCalendrier();
+    console.log(selectedDate.getFullYear())
+
     return(
-        <div className="col-8">
-            {/* Year */}
-            <div className="row">
-              
-                <div className="col-8 text-center">
-                    <span>{date.getFullYear()}</span>
-                </div>
-             
-            </div>
-            {/* Month */}
-            <div className="row">
-              
-                <div className="col-8 text-center">
-                    <span>{month[date.getMonth()]}</span>
-                </div>
-              
-            </div>
-            <table>
-                {
-                    getDaysInMonth(date.getMonth(), date.getFullYear()).map((val, index) => {
-                        let week = 1;
-                        if (index % 7 === 1) {
-                            return (
-                                <tr key={index}>
-                                    {getDaysInMonth(date.getMonth(), date.getFullYear()).map((val, index) => {
-                                        if (index > week) {
-                                            return <td key={index}>{index}</td>
-                                        }
-                                        return null;
-                                    })}
-                                </tr>
-                            )
-                        }
-                        week++;
-                        return null;
-                    })
-                }
-            </table>
-        </div>
-    )
+        <>
+        <div>
+           <a href="#" onClick={getPrevMonth}>&lt;&lt;</a>    {`${mois[selectedDate.getMonth()]}`} <a href="#" onClick={getNextMonth}>&gt;&gt;</a> 
+           <a href="#" onClick={getPrevYear}>&lt;&lt;</a>    {`${selectedDate.getFullYear()}`} <a href="#" onClick={getNextYear}>&gt;&gt;</a>  
+          <table className="table">
+            <thead>
+              <tr>
+                {jour.map(day => (
+                  <th key={day}>{day}</th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {
+                Object.values(calendarRows).map(cols => {
+                  return <tr key={cols[0].date}>
+                    {cols.map(col => (
+                      col.date === todayFormatted
+                        ? <td key={col.date} className={`${col.classes} today`}>
+                          {col.value}
+                        </td>
+                        : <td key={col.date} className={col.classes}>{col.value}</td>
+                    ))}
+                  </tr>
+                })
+              } 
+            </tbody>
+          </table>
+          </div>
+          </>
+      );
+
 }
 
-function getDaysInMonth(month, year) {
-    var date = new Date(year, month, 1);
-    var days = [];
-    while (date.getMonth() === month) {
-      days.push(new Date(date));
-      date.setDate(date.getDate() + 1);
-    }
-    return days;
-}
